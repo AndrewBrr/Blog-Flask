f8f7fa2b13df36f39461b777b9c4c7528b1be334
AndrewBrr <andrewrbar@gmail.com>
2022-08-28 13:31:48 -0300

    login/signup agregados

--- website/__init__.py
+++ website/__init__.py
@@ -12,8 +12,10 @@ def crear_app():
     app.config['SECRET_KEY'] = "holaatodos"
 
     from .views import views
+    from .auth import auth 
 
     app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
 
     return app
 
--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint("views", __name__)
 
+@views.route("/") ## Con esto aun si no escribimos una direccion de ruta, por defecto nos devolvera al portal o pagina de inicio
 @views.route("/portal")
 def portal():
-    return "Portal"
+    return render_template("portal.html", name = "Andrew")
